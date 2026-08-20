218e5c3a99533b521eb62ed52e6ac3c338e986c2
Dindz Studio <dindzstudio@gmail.com>
2026-08-20 23:12:50 +0800

    bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.audic.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
